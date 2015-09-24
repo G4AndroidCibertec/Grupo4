70f46e077470f1d751e0460e300468d51226e9d1
Aldo67 <aldosalazar@hotmail.com>
2015-09-23 21:11:18 -0500

    Correción consecuencia de la Modificación del Menu

--- app/src/main/java/grupo4/histoclici/ActivityAtencion.java
+++ app/src/main/java/grupo4/histoclici/ActivityAtencion.java
@@ -91,9 +91,9 @@ public class ActivityAtencion extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        //if (id == R.id.action_settings) {
+        //    return true;
+        // }
         if (id == R.id.action_save) {
 
             if (m_etFecha.getText().toString().trim().length() < 10) {
